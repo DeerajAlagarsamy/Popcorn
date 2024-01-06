19999cc3d8ea248f3507d6d84118570f8bc348db
DeerajALagarsamy <deeajalagarsamyol7xl9@gmail.com>
2024-01-06 19:16:08 +0530

    added useEffect hook and omdb api

--- src/main.jsx
+++ src/main.jsx
@@ -1,35 +1,29 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-// import "./index.css";
-import StarRating from "./star";
-import star from "./star";
+import App from "./App-v1.jsx";
+import "./index.css";
+// import StarRating from "./star";
+// import star from "./star";
 
-function Test() {
-  const [movieRatings, setMovieRatings] = useState(0);
-  return (
-    <div>
-      <StarRating
-        color="blue"
-        maxRating={5}
-        onSetMovieRating={setMovieRatings}
-      />
-      <p>
-        This movie was rated <b>{movieRatings}</b> stars
-      </p>
-    </div>
-  );
-}
+// function Test() {
+//   const [movieRatings, setMovieRatings] = useState(0);
+//   return (
+//     <div>
+//       <StarRating
+//         color="blue"
+//         maxRating={5}
+//         onSetMovieRating={setMovieRatings}
+//       />
+//       <p>
+//         This movie was rated <b>{movieRatings}</b> stars
+//       </p>
+//     </div>
+//   );
+// }
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    {/* <App /> */}
-    <StarRating
-      maxRating={5}
-      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
-    />
+  // <React.StrictMode>
+  <App />,
 
-    <StarRating color={"red"} size={"22"} defaultRating={5} />
-    <Test />
-  </React.StrictMode>
+  // </React.StrictMode>,
 );
